d8cbf7dd85f7c389984d2241fc1b4e03ffdf6581
meshal <imeshalfm@gmail.com>
2024-12-04 12:14:38 +0300

    structure navigation

--- App.js
+++ App.js
@@ -1,13 +1,7 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
-import CustomHeader from './src/components/CustomHeader';
-import RestaurantDetail from './src/screens/RestaurantDetail';
-import MenuItemDetail from './src/screens/MenuItemDetail';
-import Cart from './src/screens/Cart';
-import Login from './src/screens/Login';
-import Signup from './src/screens/Signup';
-import RestaurantsPage from './src/screens/Restaurants';
 import { CartProvider } from './src/context/CartContext';
+import HomeNavigation from './src/navigation/HomeNavigation/HomeNavigation';
+
 import {
   useFonts,
   Poppins_400Regular,
@@ -18,8 +12,6 @@ import {
 } from '@expo-google-fonts/poppins';
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
-
   let [fontLoaded] = useFonts({
     Poppins_400Regular,
     Poppins_500Medium,
@@ -35,31 +27,7 @@ export default function App() {
   return (
     <CartProvider>
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={'Home'}
-          screenOptions={({ route, navigation }) => ({
-            header: ({ options }) => (
-              <CustomHeader
-                title={options.title}
-                navigation={navigation}
-                absolute={options.absolute}
-                backgroundColor={options.backgroundColor}
-                showLogo={options.showLogo}
-              />
-            ),
-          })}
-        >
-          <Stack.Screen
-            name="Home"
-            component={RestaurantsPage}
-            options={{ headerShown: true, absolute: false, backgroundColor: '#d3e8d6', showLogo: true }}
-          />
-          <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} options={{ absolute: true }} />
-          <Stack.Screen name="MenuItemDetail" component={MenuItemDetail} options={{ presentation: 'modal', headerShown: false }} />
-          <Stack.Screen name="Cart" component={Cart} options={{ backgroundColor: '#1b1d21', absolute: false }} />
-          <Stack.Screen name="Login" component={Login} options={{}} />
-          <Stack.Screen name="Signup" component={Signup} options={{}} />
-        </Stack.Navigator>
+        <HomeNavigation />
       </NavigationContainer>
     </CartProvider>
   );
---
+++ src/navigation/HomeNavigation/HomeNavigation.js
@@ -0,0 +1,40 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import CustomHeader from '../../components/CustomHeader';
+import RestaurantDetail from '../../screens/RestaurantDetail';
+import MenuItemDetail from '../../screens/MenuItemDetail';
+import Cart from '../../screens/Cart';
+import Login from '../../screens/Login';
+import Signup from '../../screens/Signup';
+import RestaurantsPage from '../../screens/Restaurants';
+
+const Stack = createNativeStackNavigator();
+
+export default function HomeNavigation() {
+  return (
+    <Stack.Navigator
+      initialRouteName={'Home'}
+      screenOptions={({ route, navigation }) => ({
+        header: ({ options }) => (
+          <CustomHeader
+            title={options.title}
+            navigation={navigation}
+            absolute={options.absolute}
+            backgroundColor={options.backgroundColor}
+            showLogo={options.showLogo}
+          />
+        ),
+      })}
+    >
+      <Stack.Screen
+        name="Home"
+        component={RestaurantsPage}
+        options={{ headerShown: true, absolute: false, backgroundColor: '#d3e8d6', showLogo: true }}
+      />
+      <Stack.Screen name="RestaurantDetail" component={RestaurantDetail} options={{ absolute: true }} />
+      <Stack.Screen name="MenuItemDetail" component={MenuItemDetail} options={{ presentation: 'modal', headerShown: false }} />
+      <Stack.Screen name="Cart" component={Cart} options={{ backgroundColor: '#1b1d21', absolute: false }} />
+      <Stack.Screen name="Login" component={Login} options={{}} />
+      <Stack.Screen name="Signup" component={Signup} options={{}} />
+    </Stack.Navigator>
+  );
+}
